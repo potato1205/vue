c9f201c6e72ac0db02226946e9be4d62ba5c0262
Haoyu.Li <18844144418@163.com>
2020-05-10 22:46:41 +0800

    ing

--- vue-source-code/src/App.vue
+++ vue-source-code/src/App.vue
@@ -1,30 +1,46 @@
 <!--
  * @Author: your name
  * @Date: 2020-05-05 18:22:11
- * @LastEditTime: 2020-05-05 18:51:28
+ * @LastEditTime: 2020-05-10 14:44:57
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /learn-basic/vue-source-code/src/App.vue
  -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Form></Form>
+    <wt-carousel @swiper="swiper">
+      <div class="carousel-item">
+        <img src="https://ss3.bdstatic.com/iPoZeXSm1A5BphGlnYG/skin/822.jpg" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://ss3.bdstatic.com/iPoZeXSm1A5BphGlnYG/skin/823.jpg" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://ss3.bdstatic.com/iPoZeXSm1A5BphGlnYG/skin/824.jpg" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://ss3.bdstatic.com/iPoZeXSm1A5BphGlnYG/skin/825.jpg" />
+      </div>
+    </wt-carousel>
+    <!-- <Swiper></Swiper> -->
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Form from './components/form'
+// import Form from "./components/form";
+import Swiper from "./components/swiper/swiper";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // HelloWorld,
-    Form
+    // Form
+    'wt-carousel': Swiper
   }
-}
+};
 </script>
 
 <style>
